88488ac46ba82d56804e23504aa53b9dd68bba5c
Liam Chapman <patch5216@gmail.com>
2014-02-18 21:29:01 +0000

    some inital ideas for methods..

--- src/LoginAuth/loginauth.php
+++ src/LoginAuth/loginauth.php
@@ -4,6 +4,30 @@ namespace LoginAuth;
 
 class LoginAuth {
 
+	public function __construct () {}
 
+	public function login () {}
+
+	public function logout () {}
+
+	public function has () {}
+
+	public function is () {}
+
+	public function session () {}
+
+	public function cookie () {}
+
+	public function __set () {}
+
+	public function __get () {}
+
+	public function __call () {}
+
+	public function __callStatic () {}
+
+	public function __toString () {}
+
+	public function __clone () {}
 
 }
